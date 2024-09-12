706807462ffdc703ab195c4c1ba8e1880a8dbf1a
Henry Bley-Vroman <olets@olets.dev>
2024-09-12 12:17:30 -1000

    feat(docs): scaffold package...
    >>> mkdir packages/docs
    >>> cd packages/docs
    >>> bun init
    
    - package.json changes:
      - modify version
      - add description, author, license, homepage, repository, bugs, keywords
    - Add LICENSE
    
    >>> bun add -D vitepress
    >>> bun vitepress init
    # for the directory, say `src` instead of default `docs`
    >>> echo src/.vitepress/cache > .gitignore
    >>> cd ../..
    
    In the monorepo root package.json, modify `scripts`

---
+++ packages/docs/src/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "@olets/unocss-preset-css",
+  description: "The 'css-in-the-class-attribute' preset for UnoCSS",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
