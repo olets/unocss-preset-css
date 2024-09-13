8cd6ada0bdf668e7821d5de00ac43433cc96135f
Henry Bley-Vroman <olets@olets.dev>
2024-09-13 08:02:54 -1000

    chore(preset>build): let unbuild to infer entries from  package.json

--- packages/preset/build.config.ts
+++ packages/preset/build.config.ts
@@ -1,7 +1,6 @@
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-  entries: ["src/index"],
   clean: true,
   declaration: true,
   failOnWarn: false, // To work around the warning "Inlined implicit external @unocss/preset-attributify"
